81ddbe6d2aa4677987f55777cbab20d7771ebb8c
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2024-04-26 09:15:56 +0530

    Dynamic Navbar Added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './../assets/Advertise_Insides.svg'
 import Menu from './../assets/menu.png'
 import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <div className="backdrop-blur-[10px] fixed top-0 z-50 w-[100vw] mb-10 h-20 flex justify-between align-middle">
-        <div className=' w-[85vw] flex justify-between align-middle ml-auto mr-auto'>
-          <img className='mobile:w-[150px] tablet:w-[200px] laptop:w-[247px] desktop:w-[290px]' src={Logo} alt="Advertise_Insides.svg" />
-          <div className='flex mt-auto mb-auto'>
-            <ul className="laptop:flex tablet:flex mobile:hidden desktop:flex justify-center gap-x-5 font-sans text-xl capitalize font-['Times New Roman']">
-              <Link to="/" className="text-blue-700">
-                Home
-              </Link>
-              <Link to="/advertise">advertise</Link>
-              <Link to="/about">about</Link>
-            </ul>
-            <button className='mobile:block laptop:hidden desktop:hidden tablet:hidden'>
-              <img className='w-[40px]' src={Menu} alt='menu' />
-            </button>
+      <div className='w-100 p-6'>
+        <div className="backdrop-blur-[10px] fixed top-0 z-50 w-[100vw] mb-10 h-20 mobile:h-fit flex justify-between align-middle">
+          <div className='p-4 w-[85vw] flex justify-between align-middle ml-auto mr-auto'>
+            <img className='mobile:w-[150px] tablet:w-[200px] laptop:w-[247px] desktop:w-[290px]' src={Logo} alt="Advertise_Insides.svg" />
+            <div className='flex mt-auto mb-auto'>
+              <ul className="laptop:flex tablet:flex mobile:hidden desktop:flex justify-center gap-x-5 font-sans text-xl capitalize font-['Times New Roman']">
+                <Link to="/" className="text-blue-700">
+                  Home
+                </Link>
+                <Link to="/advertise">advertise</Link>
+                <Link to="/about">about</Link>
+              </ul>
+
+              <button className='mobile:block laptop:hidden desktop:hidden tablet:hidden'>
+                <img className='w-[40px]' src={Menu} alt='menu' onClick={toggleNavbar} />
+              </button>
+            </div>
           </div>
         </div>
+        <div className={`fixed top-20 left-10 right-10 bg-white bg-opacity-10 shadow-lg backdrop-blur-[10px] rounded-[20px] z-50 transition duration-300 ease-in-out transform ${isOpen ? 'block' : 'hidden'}`}>
+          <div className="backdrop-blur-[10px] bg-opacity-10 bg-gray-100 rounded-[10px] w-full px-3 py-4">
+            <ul className="flex flex-col justify-center gap-y-4 text-xl capitalize font-['Times New Roman']">  <Link to="/" className="text-blue-700 hover:rounded-[10px] p-2 hover:underline">
+              Home
+            </Link>
+              <Link className='hover:rounded-[10px] p-2 hover:underline' to="/advertise">advertise</Link>
+              <Link className='hover:rounded-[10px] p-2 hover:underline' to="/about">about</Link>
+            </ul>
+          </div>
+        </div>
+
       </div>
     </>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Herosection() {
         <div className="w-[90vw] mobile:h-fit laptop:w-fit laptop:h-fit bg-gradient-to-br from-20% from-blue-700 via-60% via-[color:#B1C7FF] to-96% to-blue-900 rounded-2xl">
           <img className='flex ml-auto mr-auto mt-9 laptop:w-[550px] mobile:w-[80vw] mobile:rounded-[10px]' src={Bottle} alt="Bottle-Image.jpg" />
           <div className='relative -top-[30px] laptop:left-12 laptop:-top-[70px] left-4 ml-auto mr-auto px-4 bg-[color:#2770CC] bg-opacity-80 rounded-[10px] laptop:w-[26vw] laptop:p-8 laptop:leading-10 mobile:w-[65vw] h-fit p-1'>
-            <span class="text-white mobile:text-2xl tablet:text-3xl laptop:text-[2.8rem] desktop:text-[3.0rem] font-bold font-['Kalam'] capitalize">Uplift Lives with Every Sip</span>
+            <span className="text-white mobile:text-2xl tablet:text-3xl laptop:text-[2.8rem] desktop:text-[3.0rem] font-bold font-['Kalam'] capitalize">Uplift Lives with Every Sip</span>
           </div>
 
           <div className='m-5 font-["Inter"] laptop:w-[70%] mobile:w-[90%] mr-auto ml-auto font-extrabold bg-white bg-opacity-15 p-2 rounded-[10px]'>
